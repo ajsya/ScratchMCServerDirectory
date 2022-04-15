c69a696ace28d918f610e089aee01db7f6508a4b
ajsya <52182942+ajsya@users.noreply.github.com>
2022-04-14 22:45:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, time, scratchconnect
+import os, time, json, scratchconnect
 from mcstatus import MinecraftServer
 from dotenv import load_dotenv
 from random import randint
@@ -16,14 +16,30 @@ def lookup(server_ip):
     try:
         server = MinecraftServer.lookup(server_ip)
         status = server.status()
-        query = server.query()
+        #query = server.query()
+
+        modt = status.description
+        description = modt.translate({ ord(c): None for c in "§\n" })
+
+        raw = status.raw
+        rawjson = json.dumps(raw)
+        rawdata = json.loads(str(rawjson))
+
+        players = []
+        for name in rawdata['players']['sample']:
+            print(name['name'])
+            players.append(name['name'])
+        print(players)
+        players1 = players[0:6]
+        playersample=' '.join([str(item) for item in players1])
+        print(playersample)
     
-        return randint(0, 10000), server.host, "{0}/{1}".format(status.players.online, status.players.max), status.version.name, status.latency, query.motd, query.players.names
+        return randint(0, 10000), server.host, "{0}/{1}".format(status.players.online, status.players.max), status.version.name, status.latency, description, playersample
     except:
         try:
             server = MinecraftServer.lookup(server_ip)
             status = server.status()
-            
+
             return randint(0, 10000), server.host, "{0}/{1}".format(status.players.online, status.players.max), status.version.name, status.latency
         except:
             return "Error"
@@ -41,6 +57,11 @@ def do_something(**data):
             variables.set_cloud_variable(variable_name="response", value=404)
             print("Response error sent")
         else:
-            set = variables.set_cloud_variable(variable_name="response", value=variables.encode_list(list(response)))
+            print(response) #response object needs to be sent in two pieces to avoid scratch cloud character limit
+            print(variables.encode_list(list(response)))
+            set = variables.set_cloud_variable(variable_name="response", value=variables.encode_list(list(response[0:5])))
+            set = variables.set_cloud_variable(variable_name="modt", value=variables.encode(response[5]))
+            set = variables.set_cloud_variable(variable_name="playersample", value=variables.encode(response[6]))
+
             if set:
                 print("Response sent!")
