153aa6cb42d5c08545354b48586db339d919456b
ajsya <52182942+ajsya@users.noreply.github.com>
2022-05-03 20:42:38 -0400

    major bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#self-hosting guide: https://gist.github.com/emxsys/a507f3cad928e66f6410e7ac28e2990f
+
 import os, time, json, scratchconnect
-from mcstatus import MinecraftServer
+from mcstatus import JavaServer
 from dotenv import load_dotenv
 from random import randint
 
@@ -9,12 +11,12 @@ username = os.environ['USERNAME']
 password = os.environ['PASSWORD']
 
 user = scratchconnect.ScratchConnect(username, password)
-project = user.connect_project(project_id=601861832, access_unshared=True)
+project = user.connect_project(project_id=684696037)
 variables = project.connect_cloud_variables()
 
 def lookup(server_ip):
     try:
-        server = MinecraftServer.lookup(server_ip)
+        server = JavaServer(server_ip)
         status = server.status()
         #query = server.query()
 
@@ -34,34 +36,41 @@ def lookup(server_ip):
         playersample=' '.join([str(item) for item in players1])
         print(playersample)
     
-        return randint(0, 10000), server.host, "{0}/{1}".format(status.players.online, status.players.max), status.version.name, status.latency, description, playersample
+        return randint(0, 10000), server_ip, "{0}/{1}".format(status.players.online, status.players.max), status.version.name, status.latency, description, playersample
     except:
         try:
             server = MinecraftServer.lookup(server_ip)
             status = server.status()
 
-            return randint(0, 10000), server.host, "{0}/{1}".format(status.players.online, status.players.max), status.version.name, status.latency
+            return randint(0, 10000), server_ip, "{0}/{1}".format(status.players.online, status.players.max), status.version.name, status.latency
         except:
             return "Error"
 
 event = variables.start_event(update_time=5)  # Start a cloud event loop to check events. Use the 'update_time' parameter to wait for that number of seconds and then update the data.
 
-@variables.event.on('change')
-def do_something(**data):
-    print(data['variable_name'])
-    variables = project.connect_cloud_variables()
-    if data['variable_name'] == '☁ request':
-        server_ip = variables.decode(data['value'])
-        response = lookup(server_ip)
-        if response == "Error":
-            variables.set_cloud_variable(variable_name="response", value=404)
-            print("Response error sent")
-        else:
-            print(response) #response object needs to be sent in two pieces to avoid scratch cloud character limit
-            print(variables.encode_list(list(response)))
-            set = variables.set_cloud_variable(variable_name="response", value=variables.encode_list(list(response[0:5])))
-            set = variables.set_cloud_variable(variable_name="modt", value=variables.encode(response[5]))
-            set = variables.set_cloud_variable(variable_name="playersample", value=variables.encode(response[6]))
+last_request = '0'
+while True:
 
-            if set:
-                print("Response sent!")
+    variables = project.connect_cloud_variables()
+    request = variables.get_cloud_variable_value(variable_name='request')[0]
+
+    if request != '0':
+        if request == '2':
+            print('No new requests.')
+
+        else:
+            server_ip = variables.decode(request)
+            response = lookup(server_ip)
+            if response == "Error":
+                variables.set_cloud_variable(variable_name="response", value=404)
+                print("Response error sent")
+            else:
+                print(response) #response object needs to be sent in two pieces to avoid scratch cloud character limit
+                print(variables.encode_list(list(response)))
+                set = variables.set_cloud_variable(variable_name="response", value=variables.encode_list(list(response[0:5])))
+                set = variables.set_cloud_variable(variable_name="modt", value=variables.encode(response[5]))
+                set = variables.set_cloud_variable(variable_name="playersample", value=variables.encode(response[6]))
+
+                if set:
+                    print("Response sent!")
+        time.sleep(7) #delay between requests in secs
